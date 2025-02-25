f55d9b619d2d247b3ff73e1f3bffa4a187db9340
Kavinesh <146022197+Kavinesh248@users.noreply.github.com>
2025-02-25 20:17:37 +0530

    Delete fast-react-pizza directory

--- fast-react-pizza/src/features/menu/Menu.loader.js
+++
@@ -1,6 +0,0 @@
-import { getMenu } from "../../services/apiRestaurant";
-
-export async function loader() {
-  const menu = getMenu();
-  return menu;
-}
--- fast-react-pizza/src/ui/Loader.jsx
+++
@@ -1,5 +0,0 @@
-const Loader = function () {
-  return <div className="loader"></div>;
-};
-
-export default Loader;
